d762f9cc93ea8af03c02e3a8d58cffca678821b9
cloky0000 <lyjin2002@gmail.com>
2023-10-16 02:02:19 +0900

    s

--- map/main.js
+++ map/main.js
@@ -1,17 +1,85 @@
 const express = require('express');
+const fs = require('fs')
+const ejs = require('ejs')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const client = mysql.createConnection({
+    user: 'root',
+    password: 'lyjin0928!', //본인의 db root 계정 비밀번호
+    database: 'test1' //본인의 db
+  })
 const app = express();
 
-app.set('views','./views');
-app.set('view engine','ejs');
-
-app.get("/",(req,res)=>{
-    res.render("main/home");
-});
-
-app.get('/main',(req, res) => {
-    res.render("main/mappage");
-});
+app.use(bodyParser.urlencoded({
+    extended: false
+  }))
 
 app.listen(8080, () => {
     console.log('server 8080');
 });
+
+app.set('views','./map/views');
+app.set('view engine','ejs');
+
+app.get('/', function (req, res) {
+    fs.readFile('list.ejs', 'utf8', function (err, data) {
+      client.query('select * from res', function (err, results) {
+        if (err) {
+          res.send(err)
+        } else {
+          res.send(ejs.render(data, {
+            data: results
+          }))
+        }
+      })
+    })
+  })
+
+  app.get('/main',(req,res)=>{
+    res.render("main/mappage");
+  });
+
+  app.get('/delete/:id', function (req, res) {
+    client.query('delete from res where id=?', [req.params.id], function () {
+        res.redirect('/')
+      })
+  })
+  
+  app.get('/insert', function (req, res) {
+    fs.readFile('insert.html', 'utf8', function (err, data) {
+        res.send(data)
+      })
+  })
+  
+  app.post('/insert', function (req, res) {
+    const body = req.body
+    client.query('insert into res (name, phone, address, lat, lng, infor) values (?, ?, ?, ?, ?, ?);', [
+        body.name,
+        body.phone,
+        body.address,
+        body.lat,
+        body.lng,
+        body.infor,
+      ], function() {
+        res.redirect('/')
+      })
+  })
+  
+  app.get('/edit/:id', function (req, res) {
+    fs.readFile('edit.ejs', 'utf8', function (err, data) {
+        client.query('select * from res where id = ?', [req.params.id], function (err, result) {
+          res.send(ejs.render(data, {
+            data: result[0]
+          }))
+        })
+      })
+  })
+  
+  app.post('/edit/:id', function (req, res) {
+    const body = req.body
+    client.query('update res SET name=?, phone=?, address=?, lat=?, lng=?, infor=? where id=?',[
+        body.name, body.phone, body.address, body.lat, body.lng, body.infor, req.params.id
+    ], function () {
+        res.redirect('/')
+    })
+  })
